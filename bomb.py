e61ca20b4d437a6fccca8ce1e007fb1b5fcb0194
w1268 <92883308+w1268@users.noreply.github.com>
2025-01-27 11:27:02 +0300

    stage10
    all bombs are added to the same array. The next stage will be different. 
    I think this is inflexible. There should be 3 arrays. player1,player2 and default bomb.

--- bomb.py
+++ bomb.py
@@ -1,53 +1,76 @@
 import pygame
 from box import Box
-TILE_SIZE=40
+#TILE_SIZE=40
 box=Box()
 
-turn_reduced=False     #add __init__
-turn_reduced_toggle=False     #add __init__
+first_added_bomb_from_player1=[]
+
+index_handle=0
+
+
+
+flag_first_added_from_player1=False
 
 class Bomb():
+    #def __init__(self):
+              
 
     def add_bomb_location(self,screen,bomb_info_from_player,player1_bomb_count,player2_bomb_count):
 
-        self.player1_bomb_count=player1_bomb_count     #add __init__
-        self.player2_bomb_count=player2_bomb_count     #add __init__
+        self.player1_bomb_count=player1_bomb_count    
+        self.player2_bomb_count=player2_bomb_count 
 
+        self.font25 = pygame.font.Font(None, 25)      
+        self.font15 = pygame.font.Font(None, 25)    
 
-        font25 = pygame.font.Font(None, 25)     #add __init__
-        font15 = pygame.font.Font(None, 25)     #add __init__
-
-        bomb_info_text =font25.render(f"bomb_location:{bomb_info_from_player}", True, "black")
+        bomb_info_text =self.font25.render(f"bomb_location:{bomb_info_from_player}", True, "black")
         screen.blit(bomb_info_text, (20, 50))
 
-        bomb_info_text_count = font15.render(f"p1:{self.player1_bomb_count} p2:{self.player2_bomb_count}", True, "black")
+        bomb_info_text_count = self.font15.render(f"p1:{self.player1_bomb_count} p2:{self.player2_bomb_count} size_of_bomb_array: {len(bomb_info_from_player)}", True, "black")
         screen.blit(bomb_info_text_count, (20, 500))        
 
-        global turn_reduced     #add __init__
-        global turn_reduced_toggle     #add __init__
+        global first_added_bomb_from_player1
+        global index_handle
+        global flag_first_added_from_player1
+        
 
         ##drop bomb from player events
         for index, bomb_info in enumerate(bomb_info_from_player):
-          
+
+            #(bomb X    ,bomb Y  ,bomb TURN COUNT   ,PLAYER ID)
+            #(bomb_x,   bomb_y   ,bomb_turn         ,bomb_by)
             bomb_x,bomb_y,bomb_turn,bomb_by=bomb_info
 
+        
+            if bomb_by==0:  ##default bombs      
+                pygame.draw.circle(screen,"black",(bomb_x,bomb_y),15)        
+                bomb_info_text_default = self.font15.render(f"{bomb_turn}", True, "white")
+                screen.blit(bomb_info_text_default, (bomb_x-5, bomb_y-6))
 
             if bomb_by==1: ##added bomb by player1
                 pygame.draw.circle(screen,"red",(bomb_x,bomb_y),15) 
-                bomb_info_text_default = font15.render(f"{bomb_turn}", True, "white")
+                bomb_info_text_default = self.font15.render(f"{bomb_turn}", True, "white")
                 screen.blit(bomb_info_text_default, (bomb_x-5, bomb_y-6))
+
+
+                if player1_bomb_count==1:
+                    bomb_info_text_ = self.font15.render(f"{bomb_info} {index}", True, "black")
+                    screen.blit(bomb_info_text_, (100,400))
+
+                    bomb_info_text_a = self.font15.render(f"{first_added_bomb_from_player1} {index_handle}", True, "black")
+                    screen.blit(bomb_info_text_a, (100,450))
+
+
             
             if bomb_by==2: ##added bomb by player1
                 pygame.draw.circle(screen,"blue",(bomb_x,bomb_y),15) 
-                bomb_info_text_default = font15.render(f"{bomb_turn}", True, "white")
+                bomb_info_text_default = self.font15.render(f"{bomb_turn}", True, "white")
                 screen.blit(bomb_info_text_default, (bomb_x-5, bomb_y-6))
 
-            if bomb_by==0:  ##default bombs      
-                pygame.draw.circle(screen,"black",(bomb_x,bomb_y),15)        
-                bomb_info_text_default = font15.render(f"{bomb_turn}", True, "white")
-                screen.blit(bomb_info_text_default, (bomb_x-5, bomb_y-6))            
+                        
 
-            if player1_bomb_count==3:
+            if len(bomb_info_from_player)==13:
+
 
                 bomb_directions = { #if the bomb, near to other bombs                                            
 
@@ -61,23 +84,56 @@ class Bomb():
                 for direction, (x, y) in bomb_directions.items():                        
                         for bomb in bomb_info_from_player:
 
-                            if (x, y) == (bomb[0], bomb[1]):
+                            if (x, y) == (bomb[0], bomb[1]):  
 
                                 if bomb[3]==0:  ## check for able reduce. its mean, is it default bomb?...
                                     
                                     bomb[2]-=1   
 
-                                    #bomb_info_from_player.remove(bomb_info)     #removed player's bomb                             
+                                    bomb_info_from_player.remove(bomb_info)     #removed player's bomb                             
 
-                                    bomb_info_text = font25.render(f"yakinda default bomba var", True, "blue")
+                                    bomb_info_text = self.font25.render(f"yakinda default bomba var", True, "blue")
                                     screen.blit(bomb_info_text, (300, 10))
 
                                     if bomb[2]==0:
                                         bomb_info_from_player.remove(bomb)      #removed default bomb, when turn is 0
+
+                                        #bomb_info_from_player.remove(bomb_info)
+                                        del bomb_info_from_player[(len(bomb_info_from_player))-3]
                             
+                            
+                            
+
+
+           
+                
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         #to do:
         # 
-        # if there is a player1 bomb close to the default bomb, explode or decrease the turn 
+        # if there is a player1's bomb close to the default bomb, explode or decrease the turn 
         #
         # explode added first bomb and maybe close other bomb when player's bomb count is 3
         # meabwhile decrease related default bomb turn if the turn is 0, explode it                       
